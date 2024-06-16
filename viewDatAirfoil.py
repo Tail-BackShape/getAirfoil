3d48457671640a94aa290f7a000bfad69a6cc14a
Tail_BackShape <130843271+Tail-BackShape@users.noreply.github.com>
2024-06-16 23:23:49 +0900

    fix viewDatAirfoil.py
    Appropriate variable names.

--- viewDatAirfoil.py
+++ viewDatAirfoil.py
@@ -3,13 +3,13 @@ import numpy as np
 import sys
 
 # Load data
-data03_axis1, data03_value1  = np.loadtxt(sys.argv[-1], skiprows=3, unpack=True)
+data_x, data_y  = np.loadtxt(sys.argv[-1], skiprows=3, unpack=True)
 
 
 
 fig = plt.figure(figsize=(10, 6))
 ax = fig.add_subplot(111)
-ax.plot(data03_axis1, data03_value1 , "o-", color="k", label="value1 of data01")
+ax.plot(data_x, data_y , "o-", color="k", label="value1 of data01")
 ax.set_xlim(-0.1, 1.1)
 ax.set_ylim(-0.1, 0.5)
 ax.set_xticks(np.arange(-0.125, 1.125, 0.025))
